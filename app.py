a01e28f21c51c73ee48601e6d01d2f15e493947f
Sergio-pada <pada.sergio@gmail.com>
2025-02-10 17:04:25 -0800

    Randomized card order for study. Added ability to store multi line key: value pairs

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import json
 import os
+import random
 
 
 file_path = "index_card.json";
@@ -9,23 +10,50 @@ if not os.path.exists(file_path):
         file.write("{}")
     result_dict = {}
 else:
-    print("The file '{file_path}' already exists")
 
     with open('index_card.json', 'r') as fh:
         result_dict = json.load(fh)
 
+items = list(result_dict.items())
+random.shuffle(items)
+result_dict = dict(items)
+
 command_type = input("What would you like to do(create or study): ")
 
 if command_type == 'create': 
-    key = input("Enter prompt: ")
-    value = input("Enter answer: ")
+    
+    lines = []
+    print("Enter Prompt(type 'END' when done):")
+    while True:
+        line = input()
+        if line == "END":
+            break
+        lines.append(line)
+    key = "\n".join(lines)
+
+
+    # value = input("Enter answer: ")
+    lines = []
+    print("Enter Prompt(type 'END' when done):")
+    while True:
+        line = input()
+        if line == "END":
+            break
+        lines.append(line)
+    value = "\n".join(lines)
+
     result_dict[key] = value
     with open(file_path, 'w') as file:
         json.dump(result_dict, file)
+
+
+
+
 elif command_type == "study":
+    
     for key, value in result_dict.items():
+        print("----------------------------------- \n")
         print(key)
         input("")
         print(value)
         input("")
-print("test")
